c7a451df71141f31e31824c9c44da2779d72a33e
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-06-11 23:35:14 -0300

    Added support for local images, improved footer component and updated example

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,22 @@
-module.exports = {
-    dir: {
-        input: "src",
-        output: "public",
-        data: "../data"
+const portfolio = require('./data/portfolio.json')
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addFilter('relativeLink', (value) => {
+        return portfolio.root + value
+    })
+
+    eleventyConfig.addFilter("assetLink", (value) => {
+        if(value.search('http') == -1)
+            value = `${portfolio.root}/assets/${value}`
+
+        return value
+    })
+    
+    return {
+            dir: {
+            input: "src",
+            output: "public",
+            data: "../data"
+        }
     }
 }
