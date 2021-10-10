148a145ac9609a4f7180d971498881309fd6233b
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-10-10 13:17:38 -0300

    Refactor source code

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,21 @@
 const portfolio = require('./data/portfolio.json')
 
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addFilter('relativeLink', (value) => {
-        return portfolio.root + value
-    })
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addFilter('relativeLink', (value) => portfolio.root + value)
 
-    eleventyConfig.addFilter("assetLink", (value) => {
-        if(value.search('http') == -1)
-            value = `${portfolio.root}/assets/${value}`
-
-        return value
-    })
-    
-    return {
-            dir: {
-            input: "src",
-            output: "public",
-            data: "../data"
-        }
+  eleventyConfig.addFilter('assetLink', (value) => {
+    if (value.search('http') == -1) {
+      return `${portfolio.root}/assets/${value}`
     }
+
+    return value
+  })
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+      data: '../data',
+    },
+  }
 }
