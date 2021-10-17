58c205013438c909c0d9853e04f30fc7762e275a
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-10-17 10:12:10 -0300

    Simplify eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,6 @@
 const portfolio = require('./data/portfolio.json')
 
-const getRoot = () => {
-  if (process.env.NODE_ENV === 'dev') {
-    return ''
-  }
-
-  return portfolio.root
-}
+const getRoot = () => process.argv.includes('--serve') ? '' : portfolio.root
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addFilter('relative', (value) => getRoot() + value)
