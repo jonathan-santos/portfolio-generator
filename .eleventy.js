06385d8a1140c89ff6524e78f04f6d28b3e7319d
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-11-07 11:35:52 -0300

    Add support for multiple videos per project

--- .eleventy.js
+++ .eleventy.js
@@ -2,20 +2,24 @@ const portfolio = require('./data/portfolio.json')
 
 const getRoot = () => process.argv.includes('--serve') ? '' : portfolio.root
 
+const getAssetLocation = (assetType, asset) => {
+  if (asset.search('http') == -1) {
+    return `${getRoot()}/assets/${assetType}/${asset}`
+  }
+
+  return asset
+}
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addWatchTarget('./src/styles')
 
   eleventyConfig.addFilter('relative', (value) => getRoot() + value)
 
-  eleventyConfig.addFilter('asset', (value) => `${getRoot()}/assets/${value}`)
+  eleventyConfig.addFilter('asset', (asset) => `${getRoot()}/assets/${asset}`)
 
-  eleventyConfig.addFilter('img', (value) => {
-    if (value.search('http') == -1) {
-      return `${getRoot()}/assets/img/${value}`
-    }
+  eleventyConfig.addFilter('img', (asset) => getAssetLocation('img', asset))
 
-    return value
-  })
+  eleventyConfig.addFilter('video', (asset) => getAssetLocation('video', asset))
 
   eleventyConfig.addPassthroughCopy('src/assets')
 
