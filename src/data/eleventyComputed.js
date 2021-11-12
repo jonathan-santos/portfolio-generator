e1251ec17938c64e236c74234758fb8090fbd67e
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-11-12 10:15:08 -0300

    Delete template layouts home, category and, tags. Create template layout projects in place

--- src/data/eleventyComputed.js
+++ src/data/eleventyComputed.js
@@ -1,9 +1,19 @@
 module.exports = {
   projects: (data) => {
+    let projects
+
     if (!data.lang || data.lang === data.portfolio.i18n.default) {
-      return data.portfolio.projects
+      projects = data.portfolio.projects
+    } else {
+      projects = data[data.lang].projects
     }
 
-    return data[data.lang].projects
+    if (data['filter-type'] === 'category') {
+      projects = projects.filter(project => project.category === data.filter.name)
+    } else if (data['filter-type'] === 'tag') {
+      projects = projects.filter(project => project.tags && project.tags.includes(data.filter.name))
+    }
+
+    return projects
   },
 };
