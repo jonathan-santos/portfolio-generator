b2e66e2010a7aec0d6d1662b6b4d0e4308a75328
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-11-17 09:00:41 -0300

    Improve language-selector results and, implement it in project template layout

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,33 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addFilter('translate', (key, locale) => translate(locale, key))
 
+  eleventyConfig.addShortcode('i18n-link', (url, lang, pageLang) => {
+    if (lang === pageLang) {
+      return `
+        <li class='selected' aria-hidden='true'>
+          <span>${lang}</span>
+        </li>
+      `
+    }
+
+    const separated = url.split('/').filter(u => u !== '')
+    let i18nURL
+
+    if (lang === portfolio.i18n.default) { // '/lang' => '/'
+      i18nURL = `/${separated.slice(1).join('/')}`
+    } else if (separated.length === 1 || separated.length === 3) { // '/lang/' => '/other-lang/'
+      i18nURL = `/${lang}/${separated.slice(1).join('/')}`
+    } else if (separated.length === 0 || separated.length === 2) { // '/' => '/lang/'
+      i18nURL = `/${lang}/${separated.join('/')}`
+    }
+
+    return `
+      <li>
+        <a href='${i18nURL}'>${lang}</a>
+      </li>
+    `
+  })
+
   eleventyConfig.addPassthroughCopy({ 'src/assets': 'assets' })
 
   eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
